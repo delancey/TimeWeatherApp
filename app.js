e77c31b63d8fc5a7e8bd91a14870099cffd5906e
delancey <dsdelancey@gmail.com>
2020-04-17 08:26:44 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let zip = prompt("Please enter your zip code");
+
+let zipCode = parseInt(zip);
+console.log(zipCode);
+
+let Moment = require('moment');
+
+
+
+
+   
+    var currentTime = new Moment();
+    console.log(time.format('h:mm:ss a'));
+
+    
+ 
+
